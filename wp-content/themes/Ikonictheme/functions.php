354936c6a0d459d5bac7d55933d06d6b29c4bb0a
Muhammad Zubair Jaddan <zubairmustafa1995@gmail.com>
2023-12-03 23:20:47 +0500

    Task 3:- redirect the user away from the site if their IP address starts with 77.29

---
+++ wp-content/themes/Ikonictheme/functions.php
@@ -0,0 +1,18 @@
+<?php
+
+
+// Task 3:- redirect the user away from the site if their IP address starts with 77.29
+function redirect_users_with_ip() {
+    // Current user's IP address
+    $user_ip = $_SERVER['REMOTE_ADDR'];
+
+    // IP address pattern to check
+    $ip_pattern = '77.29';
+
+    // If the user's IP address starts with the pattern
+    if (strpos($user_ip, $ip_pattern) === 0) {
+        wp_redirect('https://google.com/'); // This will redirect to Google page
+        exit();
+    }
+}
+add_action('init', 'redirect_users_with_ip');
