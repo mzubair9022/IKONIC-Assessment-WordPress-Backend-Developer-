7297a06ed3709e183e6245d8ba1a8c6cbc2010c6
Muhammad Zubair Jaddan <zubairmustafa1995@gmail.com>
2023-12-04 16:39:27 +0500

    Task 7:- Random Coffee image through API. Defined in plugin folder and display on home page

--- wp-content/themes/Ikonictheme/index.php
+++ wp-content/themes/Ikonictheme/index.php
@@ -1,9 +1,32 @@
 <?php
+get_header(); ?>
 
-get_header();
+<?php if ( is_home() && ! is_front_page() && ! empty( single_post_title( '', false ) ) ) : ?>
+	<header class="page-header alignwide">
+		<h1 class="page-title"><?php single_post_title(); ?></h1>
+	</header><!-- .page-header -->
+<?php endif; ?>
 
-echo "index.php";
+<?php
+if ( have_posts() ) {
 
-get_footer();
+	while ( have_posts() ) {
+		the_post();
+	}
 
+
+} else {
+
+	// If no content, include the "No posts found" template.
+	get_template_part( 'template-parts/content/content-none' );
+
+}
 ?>
+
+<div class="hs-coffee-container">
+    <h3>Task 7:- This is Random Coffee shortcode which is defined in main plugin file.</h3>
+    <?php echo do_shortcode("[coffee_image]"); ?>
+</div>
+
+
+<?php get_footer();
